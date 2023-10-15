5fe75a5080054d95767ee731feef5f38ef453348
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2023-10-15 12:56:17 +0300

    Memo | React

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header({ number, data }) {
+    console.log("Header component re-rendered.")
+    return <div>Header - {number}
+
+        <br />
+        <br />
+        <code>{JSON.stringify(data)}</code>
+    </div>
+
+}
+
+export default React.memo(Header)
